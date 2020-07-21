3c2612cd12a6d1e1f2b914d5e7def2da2921374b
Simon Kramer <62208545+scyreDE@users.noreply.github.com>
2020-07-21 13:27:20 +0200

    Initial commit - wp charts

---
+++ templates/sidebar.php
@@ -0,0 +1,8 @@
+<?php
+print( '<div class="promo-area">
+	<a target="_blank" href="https://1.envato.market/X3RGa"><img src="http://static.pantherius.com/modal-survey/preview-horizontal.jpg"></a>
+	<a target="_blank" href="https://1.envato.market/0DDdL"><img src="http://static.pantherius.com/wordpress-contact-form/preview-horizontal.jpg"></a>
+</div>' );
+?>
+
+
